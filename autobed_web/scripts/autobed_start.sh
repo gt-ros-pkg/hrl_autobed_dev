e37a03def29e50dfd919777e3d641412efbfd86e
Mycroft Holmes <mycroft@prmbase.(none)>
2014-10-14 23:15:41 -0700

    adding roscore and autobed server on port 11312 to avoid conflict with pr2

--- autobed_web/scripts/autobed_start.sh
+++ autobed_web/scripts/autobed_start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 source /opt/ros/fuerte/setup.bash
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/pgrice/git/
-export ROS_MASTER_URI=http://localhost:11311
-
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/mycroft/git/
+export ROS_MASTER_URI=http://localhost:11312
+roscore -p 11312 &
+sleep 10
 roslaunch autobed_web autobed_web.launch &
 sleep 10
 roslaunch autobed_engine autobed_engine.launch
