a8e16b0f292e0b05db7de00d6bd84a3e520400c1
Yash Chitalia <yashc@gorilla.(none)>
2014-11-21 14:41:38 -0500

    Programmatically added the human to the autobed environment and ensured that the pressure map is still good.

--- hrl_gazebo_autobed/start_autobed.sh
+++ hrl_gazebo_autobed/start_autobed.sh
@@ -6,5 +6,11 @@ sleep 6
 gazebo ./autobed.world &
 sleep 6 
 
+echo "=================================================="
+echo "Initializing Human"
+echo "=================================================="
+gzfactory spawn -f /home/yashc/.gazebo/models/new_ragdoll/correct_ragdoll_original.sdf -m new_ragdoll -x 2.10 -y 0 -z 0.8 -R 1.570 -P 0 -Y 3.14
+
+sleep 6 
 roslaunch ./launch/autobed_default_controllers.launch
 
