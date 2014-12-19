f2a740e133bc5e839fffb12be3d9837396adbf61
mrich7 <mrich7@gatech.edu>
2014-12-19 17:03:56 +0000

    updating autobed start script to start automatically on raspberry pi

--- autobed_web/scripts/autobed_start.sh
+++ autobed_web/scripts/autobed_start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "Beginning of AutoBed Start Script"
 date
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 source /opt/ros/fuerte/setup.bash
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/mycroft/git/
-export ROS_MASTER_URI=http://localhost:11312
-roscore -p 11312 &
-sleep 10
-roslaunch autobed_web autobed_web.launch &
-sleep 10
-roslaunch autobed_engine autobed_engine.launch
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/pi/git
+
+sleep 1
+roslaunch /home/pi/git/hrl_autobed_dev/autobed_web/launch/autobed_web.launch &
+sleep 5 
+roslaunch /home/pi/git/hrl_autobed_dev/autobed_engine/launch/autobed_engine.launch &
