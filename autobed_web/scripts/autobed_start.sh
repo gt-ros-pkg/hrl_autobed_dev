f41dee96ef9b177b8add066285ceffc9542d4356
HRL Autobed Pi <phillip.grice@gatech.edu>
2015-01-07 17:01:57 -0500

    including launch file for autobed_engine in autobed_web, removing separate launches from startup script

--- autobed_web/scripts/autobed_start.sh
+++ autobed_web/scripts/autobed_start.sh
@@ -7,15 +7,15 @@ source /opt/ros/fuerte/setup.bash
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/pi/git
 
 echo "<<<<< ENV SETUP Done <<<<<<<<<"
-date
-sleep 5
+sleep 10 
 
 echo "<<<<< Starting Autobed Web Roslaunch <<<<"
+date
 roslaunch /home/pi/git/hrl_autobed_dev/autobed_web/launch/autobed_web.launch &
 echo "<<<<<< Autobed Web launched <<<<<<"
-date
-sleep 5 
+#sleep 10 
 
-echo "<<<<< Starting Autobed Engine Roslaunch <<<<"
-roslaunch /home/pi/git/hrl_autobed_dev/autobed_engine/launch/autobed_engine.launch &
-echo "<<<<< Autobed Engine launched <<<<"
+#echo "<<<<< Starting Autobed Engine Roslaunch <<<<"
+#date
+#roslaunch /home/pi/git/hrl_autobed_dev/autobed_engine/launch/autobed_engine.launch &
+#echo "<<<<< Autobed Engine launched <<<<"
