f8edc79fb56cdfab6d5b2b529f35c26d4134a82d
mrich7 <mrich7@gatech.edu>
2015-01-07 12:42:34 -0800

    updated start file from Henry Evans home

--- autobed_web/scripts/autobed_start.sh
+++ autobed_web/scripts/autobed_start.sh
@@ -6,7 +6,16 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 source /opt/ros/fuerte/setup.bash
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/pi/git
 
-sleep 1
+echo "<<<<< ENV SETUP Done <<<<<<<<<"
+date
+sleep 5
+
+echo "<<<<< Starting Autobed Web Roslaunch <<<<"
 roslaunch /home/pi/git/hrl_autobed_dev/autobed_web/launch/autobed_web.launch &
+echo "<<<<<< Autobed Web launched <<<<<<"
+date
 sleep 5 
+
+echo "<<<<< Starting Autobed Engine Roslaunch <<<<"
 roslaunch /home/pi/git/hrl_autobed_dev/autobed_engine/launch/autobed_engine.launch &
+echo "<<<<< Autobed Engine launched <<<<"
