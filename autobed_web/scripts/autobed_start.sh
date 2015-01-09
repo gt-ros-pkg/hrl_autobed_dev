001b04e99a63b166005bb5371ea359bcb60afed3
HRL Autobed Pi <phillip.grice@gatech.edu>
2015-01-09 15:13:01 -0500

    shortening autobed start script

--- autobed_web/scripts/autobed_start.sh
+++ autobed_web/scripts/autobed_start.sh
@@ -4,16 +4,6 @@ echo "Beginning of AutoBed Start Script"
 date
 source /opt/ros/fuerte/setup.bash
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/pi/git
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-sleep 1
-
-echo "<<<<< Starting Autobed Web Roslaunch <<<<"
-date
+echo "====== Starting Autobed Web Roslaunch ======="
 roslaunch /home/pi/git/hrl_autobed_dev/autobed_web/launch/autobed_web.launch &
-echo "<<<<<< Autobed Web launched <<<<<<"
-#sleep 10 
-
-#echo "<<<<< Starting Autobed Engine Roslaunch <<<<"
-#date
-#roslaunch /home/pi/git/hrl_autobed_dev/autobed_engine/launch/autobed_engine.launch &
-#echo "<<<<< Autobed Engine launched <<<<"
+echo "<<<<<< End of AutoBed Start Script <<<<<<"
