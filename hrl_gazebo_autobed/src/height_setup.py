206ca9ea6d5847902db2378db8987be91cbc93e5
Yash Chitalia <yashc@gorilla.(none)>
2015-03-02 21:11:21 -0500

    Improved the shell script and init script for the gazebo autobed

--- hrl_gazebo_autobed/src/height_setup.py
+++ hrl_gazebo_autobed/src/height_setup.py
@@ -9,32 +9,37 @@ class JointTrajectoryTest():
     def __init__(self, controller='/autobed_height_controller'):
         self.controller = controller
         self.goal_pub = rospy.Publisher(controller+'/command', JointTrajectory)
-        self.state_sub = rospy.Subscriber(controller+'/state', JointTrajectoryControllerState, self.state_cb)
+        self.state_sub = rospy.Subscriber(controller+'/state', 
+                JointTrajectoryControllerState, self.state_cb)
         self.joint_names = None
 
     def state_cb(self, state_msg):
         if self.joint_names is None:
             self.joint_names = state_msg.joint_names
 
-    def up_msg(self):
+    def up_msg(self, angle):
         jtm = JointTrajectory()
         jtm.joint_names = self.joint_names
         jtp = JointTrajectoryPoint()
-        jtp.positions = [0.2]*len(self.joint_names)
+        jtp.positions = [angle]*len(self.joint_names)
         jtp.time_from_start = rospy.Duration(1.0)
         jtm.points = [jtp]
         return jtm
 
-    def run(self):
+    def run(self, angle):
         while self.joint_names is None:
             print "Waiting for joint state information from %s/state topic" %self.controller
             rospy.sleep(2)
         print "Received joint state information. Sending bed to default position (0.3m)"
-        up_msg = self.up_msg()
+        up_msg = self.up_msg(angle)
         self.goal_pub.publish(up_msg)
         return
 
 if __name__=='__main__':
     rospy.init_node('autobed_height_setup')
     JTT = JointTrajectoryTest()
-    JTT.run()
+    JTT.run(0.3)
+    JTT_h = JointTrajectoryTest(controller='/autobed_head_controller')
+    JTT_h.run(0.0)
+    JTT_l = JointTrajectoryTest(controller='/autobed_legs_controller')
+    JTT_l.run(0.0)
--- hrl_gazebo_autobed/start_autobed.sh
+++ hrl_gazebo_autobed/start_autobed.sh
@@ -1,37 +1,9 @@
 #!/bin/bash
-roslaunch /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/launch/autobed_param_upload.launch &
-sleep 6 
+roslaunch ./launch/autobed_param_upload.launch  
 
-rosrun autobed_pose_estimator training_database_generator.py 1 /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/autobed_pose_estimator/database/training_data2.p &
-sleep 6 
+gazebo ./autobed.world &
 
-
-gazebo /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/autobed.world &
-sleep 6 
-
-name="new_ragdoll"
-echo "=================================================="
-echo "Tweaking Initial poses for Human" 
-echo "=================================================="
-python /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/autobed_pose_estimator/src/model_plugin_modifier.py 1 /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/autobed_pose_estimator/src/human_poses_list.p 
-rm /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/gazebo_model_plugin/ros_ragdoll_model2_plugin.cc
-mv /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/gazebo_model_plugin/ros_ragdoll_model3_plugin.cc /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/gazebo_model_plugin/ros_ragdoll_model2_plugin.cc
-rm /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/correct_ragdoll_original.sdf
-mv /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/correct_ragdoll_temporary.sdf /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/correct_ragdoll_original.sdf
-
-echo "=================================================="
-echo "Initializing Human"
-echo "=================================================="
-rm -rf /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/gazebo_model_plugin/build 
-mkdir /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/gazebo_model_plugin/build
-cd /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/gazebo_model_plugin/build  
-cmake ..
-make 
-cd /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/
-gzfactory spawn -f /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/sdf/new_ragdoll/correct_ragdoll_original.sdf -m $name 
- 
-sleep 6 
-roslaunch /home/yashc/fuerte_workspace/sandbox/git/hrl_autobed_dev/hrl_gazebo_autobed/launch/autobed_default_controllers.launch &
+roslaunch ./launch/autobed_default_controllers.launch 
 
 sleep 30 
 #kill -9 `ps aux | grep ros | awk "{print $2}"`  
