a2d951592d1bdb704112efba274014bdc5267268
HRL Autobed Pi <phillip.grice@gatech.edu>
2015-06-02 12:12:49 -0400

    updates to new server, install script, and web interface to show lost connections.

--- web_only/www/autobed.js
+++ web_only/www/autobed.js
@@ -2,59 +2,31 @@ var AutoBed = (function () {
     'use strict';
 
     var logTimer = null;
-    var log = function (txt) {
+    var log = function (txt, permanent) {
+	permanent = (permanent === undefined) ? False : permanent;
         var logDiv =  document.getElementById('log');
         logDiv.innerHTML = txt;
         if (logTimer !== null) {
             clearTimeout(logTimer);
         }
-//        logTimer = setTimeout( function () {logDiv.innerHTML = 'Ready';}, 3000);
+	if (!permanent) {
+		logTimer = setTimeout( function () {logDiv.innerHTML = 'Ready';}, 3000);
+	}
     }
-
-    var handleAJAXResponse = function (event) {
-        if (this.readyState < 4) {
-           console.log("State", this.readyState);
-	} else if (this.readyState === 4) {
-            console.log("AJAX Request Finished, response received: ", this.responseText);
-            if (this.status === 200) {
-                log( this.responseText );
-            } else {
-                log( "Error sending command." );
-            }
-        }
-    };
 
     var sendOKFlag = false;
     var sendTimer = null;
 
-    var sendAJAX = function (cmd) {
-        var cmdTxt = "cmd="+cmd;
-        var xhr;
-        if (window.XMLHttpRequest) { //Modern Browsers, IE7+
-            xhr = new XMLHttpRequest();
-         } else { // IE5, IE6
-            xhr = new ActiveXObject("Microsoft.XMLHTTP"); 
-         }
-         xhr.open("POST", "cgi-bin/autobed_controller.cgi", true);
-         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-         xhr.onreadystatechange = handleAJAXResponse;
-         xhr.send(cmdTxt);
-    };
-
     var sendCmd = function (cmd) {
         if (sendOKFlag) {
-            sendAJAX(cmd);
-            sendTimer = setTimeout(function(){ sendCmd(cmd) }, 250)
+            ws.send(cmd.toString())
+            sendTimer = setTimeout(function(){ sendCmd(cmd) }, 100)
         } else {
             clearTimeout(sendTimer);
         }
     }
 
     var setupButtonCBs = function (buttonElement) {
-//        buttonElement.addEventListener("click", function (event) {
-//            sendCmd(event.target.value);
-//        });
-
         buttonElement.addEventListener("mouseup", function(event){
                     sendOKFlag = false;
                     clearTimeout(sendTimer);
@@ -73,26 +45,38 @@ var AutoBed = (function () {
                     });
     };
 
-    var init = function () {
+    var onOpen = function () {
         setupButtonCBs(document.getElementById("head-up"));
         setupButtonCBs(document.getElementById("bed-up"));
         setupButtonCBs(document.getElementById("legs-up"));
         setupButtonCBs(document.getElementById("head-down"));
         setupButtonCBs(document.getElementById("bed-down"));
         setupButtonCBs(document.getElementById("legs-down"));
+        log("Connected to AutoBed.")
+    }
+
+    var onMessage = function (msg) {
+        console.log("WS Received: ", msg);
+    }
+
+    var onClose = function () {
+        console.log("Cannot connect to Autobed Server");
+	log("Cannot connect to Autobed Server.", true)
+    }
+
+    var ws;
+
+    var init = function () {
+        if ("WebSocket" in window) {
+	    ws = new WebSocket("ws://"+window.location.host+":828");
+	    ws.onopen = onOpen;
+	    ws.onmessage = onMessage;
+	    ws.onclose = onClose;
+        } else {
+            alert("This browser does not support websockts, which are required for AutoBed operation.")
+        }
     };
 
     return {init: init};
 
 }());
-
-AutoBed.sendDiffCmd = function (string) {
-    if (AutoBed.send) {
-        AutoBed.controlsPub.publish({'data':string});
-        AutoBed.diffTimer = setTimeout(function () {
-                AutoBed.sendDiffCmd(string)}
-                , 250)
-    } else {
-        clearTimeout(AutoBed.diffTimer);
-    }
-};
