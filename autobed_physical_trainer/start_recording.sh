92cf1164ea1903a416dad507723860fa01ef2826
Yash Chitalia <yashc@gatech.edu>
2015-06-15 17:54:09 -0400

    changed recording script

--- autobed_physical_trainer/start_recording.sh
+++ autobed_physical_trainer/start_recording.sh
@@ -2,19 +2,21 @@
 
 echo "***************** Beginning trial for file: $1 **********************"
 #roslaunch openni_launch openni.launch 
-rosrun fsa_mat_64 fsascan &
-rosrun ros_vrpn_client ros_vrpn_client __name:=mat_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=head_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=l_hand_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=r_hand_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun fsa_mat_64 fsascan &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=mat_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=head_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=l_hand_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=r_hand_o _vrpn_server_ip:=128.61.143.164 &
 #rosrun ros_vrpn_client ros_vrpn_client __name:=l_elbow_o _vrpn_server_ip:=128.61.143.164 &
 #rosrun ros_vrpn_client ros_vrpn_client __name:=r_elbow_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=l_knee_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=r_knee_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=l_ankle_o _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=r_ankle_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=l_knee_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=r_knee_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=l_ankle_o _vrpn_server_ip:=128.61.143.164 &
+#rosrun ros_vrpn_client ros_vrpn_client __name:=r_ankle_o _vrpn_server_ip:=128.61.143.164 &
 #rosrun ros_vrpn_client ros_vrpn_client __name:=torso_o _vrpn_server_ip:=128.61.143.164 &
 
+rosrun ros_vrpn_client ros_vrpn_client __name:=abd_head_angle _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=abd_leg_angle _vrpn_server_ip:=128.61.143.164 &
 sleep 5
 echo "10........."
 sleep 1
@@ -40,4 +42,4 @@ echo "***************** Starting Recording Be Ready ***********************"
 
 #rosbag record -O ./bagfiles/$1 /fsascan /head_o/pose /mat_o/pose /l_hand_o/pose /r_hand_o/pose /l_knee_o/pose /r_knee_o/pose /l_ankle_o/pose /r_ankle_o/pose /abdout0 /torso_o/pose /l_elbow_o/pose /r_elbow_o/pose 
 
-rosrun autobed_physical_trainer moving_bed_pose_trainer.py $1
+#rosrun autobed_physical_trainer moving_bed_pose_trainer.py $1
