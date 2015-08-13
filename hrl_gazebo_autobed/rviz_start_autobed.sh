27d7b38a10f3f79c54d17509651b09750a4ae617
Yash Chitalia <yashc@gatech.edu>
2015-08-13 18:03:42 -0400

    bugfix

--- hrl_gazebo_autobed/rviz_start_autobed.sh
+++ hrl_gazebo_autobed/rviz_start_autobed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #roslaunch ./launch/autobed_param_upload.launch  
 #roslaunch ./launch/display.launch
-roslaunch hrl_gazebo_autobed rviz_start.launch
+roslaunch hrl_gazebo_autobed rviz_start.launch &
 rosrun image_view image_view image:=/camera/rgb/image_color
