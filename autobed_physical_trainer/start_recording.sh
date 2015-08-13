8646e5e17e11c8c9ef91ce3ce26383b66d8f37ff
Yash Chitalia <yashc@gatech.edu>
2015-08-13 11:39:50 -0400

    Added readme, made things nicer

--- autobed_physical_trainer/start_recording.sh
+++ autobed_physical_trainer/start_recording.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
 echo "***************** Beginning trial for file: $1 **********************"
-#roslaunch openni_launch openni.launch 
-#rosrun fsa_mat_64 fsascan &
+echo "################ BOOTING MOTION CAPTURE SENSORS #####################"
 #rosrun ros_vrpn_client ros_vrpn_client __name:=mat_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=head_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=l_hand_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=r_hand_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=l_elbow_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=r_elbow_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=l_knee_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=r_knee_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=l_ankle_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=r_ankle_o _vrpn_server_ip:=128.61.143.164 &
-#rosrun ros_vrpn_client ros_vrpn_client __name:=torso_o _vrpn_server_ip:=128.61.143.164 &
-
-rosrun ros_vrpn_client ros_vrpn_client __name:=abd_head_angle _vrpn_server_ip:=128.61.143.164 &
-rosrun ros_vrpn_client ros_vrpn_client __name:=abd_leg_angle _vrpn_server_ip:=128.61.143.164 &
-#sleep 5
+rosrun ros_vrpn_client ros_vrpn_client __name:=head_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=l_hand_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=r_hand_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=l_elbow_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=r_elbow_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=l_knee_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=r_knee_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=l_ankle_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=r_ankle_o _vrpn_server_ip:=128.61.143.164 &
+rosrun ros_vrpn_client ros_vrpn_client __name:=torso_o _vrpn_server_ip:=128.61.143.164 &
+echo "####################################################################"
+echo "####################################################################"
+echo "BOOTED SENSORS. WILL BEGIN RECORDING IN 10 SECONDS"
+sleep 5
 echo "10........."
-#sleep 1
+sleep 1
 echo "9........."
-#sleep 1
+sleep 1
 echo "8........"
-#sleep 1
+sleep 1
 echo "7......."
-#sleep 1
+sleep 1
 echo "6......"
-#sleep 1
+sleep 1
 echo "5....."
-#sleep 1
+sleep 1
 echo "4...."
-#sleep 1
+sleep 1
 echo "3..."
-#sleep 1
+sleep 1
 echo "2.."
-#sleep 1
+sleep 1
 echo "1."
-#sleep 1
+sleep 1
 echo "***************** Starting Recording Be Ready ***********************"
 
-#rosbag record -O ./bagfiles/$1 /fsascan /head_o/pose /mat_o/pose /l_hand_o/pose /r_hand_o/pose /l_knee_o/pose /r_knee_o/pose /l_ankle_o/pose /r_ankle_o/pose /abdout0 /torso_o/pose /l_elbow_o/pose /r_elbow_o/pose 
+rosbag record -O ./bagfiles/$1 /fsascan /camera/rgb/image_color /head_o/pose /mat_o/pose /l_hand_o/pose /r_hand_o/pose /l_knee_o/pose /r_knee_o/pose /l_ankle_o/pose /r_ankle_o/pose /abdout0 /torso_o/pose /l_elbow_o/pose /r_elbow_o/pose 
 
-rosrun autobed_physical_trainer moving_bed_pose_trainer.py $1
--- hrl_gazebo_autobed/rviz_start_autobed.sh
+++ hrl_gazebo_autobed/rviz_start_autobed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-roslaunch ./launch/autobed_param_upload.launch  
-roslaunch ./launch/display.launch
-
+#roslaunch ./launch/autobed_param_upload.launch  
+#roslaunch ./launch/display.launch
+roslaunch hrl_gazebo_autobed rviz_start.launch
 
