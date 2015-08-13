c15a4f92093de02dd864e978d5e03a5a466413f6
Yash Chitalia <yashc@gatech.edu>
2015-08-13 17:37:13 -0400

    Bugfix for sensor boot script

--- autobed_physical_trainer/boot_autobed_sensors.sh
+++ autobed_physical_trainer/boot_autobed_sensors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-roslaunch openni_launch openni.launch 
+roslaunch openni_launch openni.launch &
 rosrun fsa_mat_64 fsascan &
 rosrun ros_vrpn_client ros_vrpn_client __name:=abd_head_angle _vrpn_server_ip:=128.61.143.164 &
 rosrun ros_vrpn_client ros_vrpn_client __name:=abd_leg_angle _vrpn_server_ip:=128.61.143.164 &
