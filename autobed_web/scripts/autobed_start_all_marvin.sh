79178aede0be230efc2a03a7e3f3b2a29696173e
Ari <akapusta@gatech.edu>
2016-12-16 16:51:47 -0500

    fixes!

--- autobed_web/scripts/autobed_start_all_marvin.sh
+++ autobed_web/scripts/autobed_start_all_marvin.sh
@@ -4,18 +4,22 @@
 
 source ~/.bashrc
 
-roscore &
 
-sleep 5
+
+#sleep 5
 
 #roslaunch autobed_web autobed_marvin.launch &
 #~/ros_workspace/git/fsa_mat_64/bin/fsascan &
 
 screen -dmS autobed-marvin
-screen -S autobed-marvin -p 0 -X stuff "roslaunch autobed_web autobed_marvin.launch
+screen -S autobed-marvin -p 0 -X stuff "roscore
+"
+sleep 5
+screen -S autobed-marvin -X screen -t hokuyo
+screen -S autobed-marvin -p hokuyo -X stuff "roslaunch autobed_web autobed_marvin.launch
 "
 screen -S autobed-marvin -X screen -t fsascan
-screen -S autobed-marvin -p 0 -X stuff "~/ros_workspace/git/fsa_mat_64/bin/fsascan
+screen -S autobed-marvin -p fsascan -X stuff "~/ros_workspace/git/fsa_mat_64/bin/fsascan
 "
 ssh pi@axiom "~/git/hrl_autobed_dev/autobed_web/scripts/autobed_start_pi.sh"
 # 'screen -dmS autobed-pi; screen -S autobed-pi -p 0 -X stuff "~/git/hrl_autobed_dev/autobed_web/scripts/autobed_start_pi.sh
