01f2b2b904ca8033f809cb865eba267877ce4329
Ari <akapusta@gatech.edu>
2016-12-16 15:15:33 -0500

    test file

---
+++ autobed_web/scripts/autobed_start_marvin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+~/ros_workspace/git/hrl_autobed_dev/autobed_web/scripts/autobed_kill_marvin.sh
+
+source ~/.bashrc
+
+roscore &
+
+sleep 5
+
+roslaunch autobed_web autobed_marvin.launch &
+~/ros_workspace/git//fsa_mat_64/bin/fsascan &
+
+ssh pi@axiom 'screen -dmS autobed-pi ~/git/hrl_autobed_dev/autobed_web/scripts/autobed_start_pi.sh'
---
+++ autobed_web/scripts/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#ssh pi@axiom 'cd ~/git/hrl_autobed_dev/autobed_web/scripts/; touch test_file'
+
+#./autobed_kill.sh
+
+#echo "this is a test"
