47c5b8fbeff3c618e629013b25ca63de84f136fa
mycroft <mycroft@mac-mini-marvin.(none)>
2016-12-16 16:05:20 -0500

    test

--- autobed_web/scripts/test.sh
+++ autobed_web/scripts/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #ssh pi@axiom 'cd ~/git/hrl_autobed_dev/autobed_web/scripts/; touch test_file'
+touch ~/ros_workspace/git/hrl_autobed_dev/autobed_web/scripts/test_file
 
 #./autobed_kill.sh
 
-#echo "this is a test"
+echo "this is a test"
