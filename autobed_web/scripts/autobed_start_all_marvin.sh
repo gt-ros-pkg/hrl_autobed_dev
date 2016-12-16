6f62222115c51fecbddec00850062a421af9df7b
Ari <akapusta@gatech.edu>
2016-12-16 15:10:41 -0500

    testing things

--- autobed_web/scripts/autobed_start_all_marvin.sh
+++ autobed_web/scripts/autobed_start_all_marvin.sh
@@ -5,9 +5,10 @@
 source ~/.bashrc
 
 roscore &
-ssh pi@axiom 'cd ~/git/hrl_autobed_dev/autobed_web/scripts/; ./autobed_start_pi.sh' &
 
 sleep 5
 
 roslaunch autobed_web autobed_marvin.launch &
 ~/ros_workspace/git//fsa_mat_64/bin/fsascan &
+
+ssh pi@axiom 'screen -dmS autobed-pi ~/git/hrl_autobed_dev/autobed_web/scripts/autobed_start_pi.sh'
